8ad59902af4271aecbc5a00d44b7bd4d53d92347
AleXXX <mail@alexxx.ru>
2026-03-13 23:21:02 +0300

    LilyGO-Mini-Epaper-S3

---
+++ variants/lilygo_mini_epaper_s3/LilyGoMiniEpaperS3.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Arduino.h>
+#include <helpers/ESP32Board.h>
+
+class LilyGoMiniEpaperS3 : public ESP32Board {
+public:
+  LilyGoMiniEpaperS3() { }
+
+  const char* getManufacturerName() const override {
+    return "LilyGo Mini Epaper S3";
+  }
+};
