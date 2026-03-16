33da47747ef17d51136e4515872c763c3c29b422
AleXXX <mail@alexxx.ru>
2026-03-16 13:21:28 +0300

    LilyGO-Mini-Epaper-S3

--- variants/lilygo_mini_epaper_s3/LilyGoMiniEpaperS3.cpp
+++ variants/lilygo_mini_epaper_s3/LilyGoMiniEpaperS3.cpp
@@ -1,4 +1,25 @@
 #include <Arduino.h>
 #include "LilyGoMiniEpaperS3.h"
 
+uint16_t LilyGoMiniEpaperS3::getBattMilliVolts() {
+  analogReadResolution(12);
 
+  uint32_t raw = 0;
+  for (int i = 0; i < 8; i++) {
+    raw += analogReadMilliVolts(PIN_VBAT_READ);
+  }
+  raw = raw / 8;
+
+  return (2 * raw);
+}
+
+void LilyGoMiniEpaperS3::begin() {
+  btn_prev_state = HIGH;
+
+  pinMode(PIN_VBAT_READ, INPUT);
+
+  // Set all button pins to INPUT_PULLUP
+  pinMode(PIN_USER_BTN, INPUT_PULLUP);
+  pinMode(ENCODER_LEFT, INPUT_PULLUP);
+  pinMode(ENCODER_RIGHT, INPUT_PULLUP);
+}
--- variants/lilygo_mini_epaper_s3/LilyGoMiniEpaperS3.h
+++ variants/lilygo_mini_epaper_s3/LilyGoMiniEpaperS3.h
@@ -4,10 +4,14 @@
 #include <helpers/ESP32Board.h>
 
 class LilyGoMiniEpaperS3 : public ESP32Board {
+protected:
+  uint8_t btn_prev_state;
 public:
   LilyGoMiniEpaperS3() { }
 
   const char* getManufacturerName() const override {
     return "LilyGo Mini Epaper S3";
   }
+  void begin();
+  uint16_t getBattMilliVolts() override;
 };
